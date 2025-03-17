33d4adf42e306148a04920e58c4159cbc84d4c27
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-03-16 22:19:46 -0400

    added shots details of Stephen Curry using shotchartdetails, commonplayerinfo and playerindex endpoints

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import playerindex
 
-# Get the player career stats for LeBron James
-career = playercareerstats.PlayerCareerStats(player_id='2544')
-print(career.get_data_frames()[0])
+player_index = playerindex.PlayerIndex().get_normalized_dict()['PlayerIndex']
+player_id = None
+for player in player_index:
+    if player['PLAYER_LAST_NAME'] == 'Curry' and player["PLAYER_FIRST_NAME"] == 'Stephen':
+        player_id = player['PERSON_ID']
+        break
+
+player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()['CommonPlayerInfo'][0]
+team_id = player_info['TEAM_ID']
+
+
+shot_details = shotchartdetail.ShotChartDetail(team_id=team_id, player_id=player_id).get_normalized_dict()['Shot_Chart_Detail']
+i = 1
+for shot in shot_details:
+    print('\n')
+    print(f"Shot detail {i}")
+    for key, val in shot.items():
+        print(f"{key}: {val}")
+
+    i += 1
+
