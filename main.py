438ddcecca483a928ea6f12a4136e20b5dbb94d0
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-04-05 23:36:54 -0400

    organized everything in functions for better readability and reuse

--- main.py
+++ main.py
@@ -4,41 +4,50 @@ from nba_api.stats.endpoints import playerindex
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
-player_index = playerindex.PlayerIndex().get_normalized_dict()['PlayerIndex']
-player_id = None
-for player in player_index:
-    if player['PLAYER_LAST_NAME'] == 'Curry' and player["PLAYER_FIRST_NAME"] == 'Stephen':
-        player_id = player['PERSON_ID']
-        break
+def get_player_id(firstname, lastname):
+    player_index = playerindex.PlayerIndex().get_normalized_dict()['PlayerIndex']
+    for player in player_index:
+        if player['PLAYER_LAST_NAME'] == lastname and player["PLAYER_FIRST_NAME"] == firstname:
+            return player['PERSON_ID']
 
-player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()['CommonPlayerInfo'][0]
-team_id = player_info['TEAM_ID']
+def get_team_id(player_id):
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()['CommonPlayerInfo'][0]
+    return player_info['TEAM_ID']
 
+def get_made_shots(player_id, team_id):
+    shot_details = shotchartdetail.ShotChartDetail(team_id=team_id, player_id=player_id).get_normalized_dict()['Shot_Chart_Detail']
+    # Filter only made shots
+    made_shots_x = [shot['LOC_X'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
+    made_shots_y = [shot['LOC_Y'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
+    return made_shots_x, made_shots_y
 
-shot_details = shotchartdetail.ShotChartDetail(team_id=team_id, player_id=player_id).get_normalized_dict()['Shot_Chart_Detail']
+def shot_chart(x, y):
+    court_img = mpimg.imread("court.png")  # Ensure court.png is in the same folder
 
+    # Plot made shots using matplotlib
+    plt.figure(figsize=(10, 10))
 
-# Filter only made shots
-made_shots_x = [shot['LOC_X'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
-made_shots_y = [shot['LOC_Y'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
+    # Set court dimensions
+    plt.xlim(-250, 250)
+    plt.ylim(-50, 420)
 
-court_img = mpimg.imread("court.png")  # Ensure court.png is in the same folder
+    plt.imshow(court_img, extent=[-250, 250, -50, 420], zorder=0)
 
-# Plot made shots using matplotlib
-plt.figure(figsize=(10, 10))
+    plt.scatter(x, y, c='green', alpha=0.5, label='Made Shots')
 
-# Set court dimensions
-plt.xlim(-250, 250)
-plt.ylim(-50, 420)
+    plt.title("Stephen Curry - Made Shots")
+    plt.xlabel("Court X")
+    plt.ylabel("Court Y")
+    plt.legend()
+    # plt.grid(True)
+    plt.axis('equal')
+    plt.show()
 
-plt.imshow(court_img, extent=[-250, 250, -50, 420], zorder=0)
+if __name__ == "__main__":
+    firstname = "Stephen"
+    lastname = "Curry"
 
-plt.scatter(made_shots_x, made_shots_y, c='green', alpha=0.5, label='Made Shots')
-
-plt.title("Stephen Curry - Made Shots")
-plt.xlabel("Court X")
-plt.ylabel("Court Y")
-plt.legend()
-# plt.grid(True)
-plt.axis('equal')
-plt.show()
+    player_id = get_player_id(firstname, lastname)
+    team_id = get_team_id(player_id)
+    made_shots_x, made_shots_y = get_made_shots(player_id, team_id)
+    shot_chart(made_shots_x, made_shots_y)
