30e67c661d95dc9da8093a0712647fa0f885b868
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-04-05 23:27:04 -0400

    implemented the shot chart analysis with matplotlib and added the court chart inside of the shot chart analysis

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.endpoints import playerindex
 from matplotlib import pyplot as plt
-from matplotlib import image as mpimg
+import matplotlib.image as mpimg
 
 player_index = playerindex.PlayerIndex().get_normalized_dict()['PlayerIndex']
 player_id = None
@@ -16,11 +16,29 @@ team_id = player_info['TEAM_ID']
 
 
 shot_details = shotchartdetail.ShotChartDetail(team_id=team_id, player_id=player_id).get_normalized_dict()['Shot_Chart_Detail']
-i = 1
-for shot in shot_details:
-    print('\n')
-    print(f"Shot detail {i}")
-    for key, val in shot.items():
-        print(f"{key}: {val}")
 
-    i += 1
+
+# Filter only made shots
+made_shots_x = [shot['LOC_X'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
+made_shots_y = [shot['LOC_Y'] for shot in shot_details if shot['SHOT_MADE_FLAG'] == 1]
+
+court_img = mpimg.imread("court.png")  # Ensure court.png is in the same folder
+
+# Plot made shots using matplotlib
+plt.figure(figsize=(10, 10))
+
+# Set court dimensions
+plt.xlim(-250, 250)
+plt.ylim(-50, 420)
+
+plt.imshow(court_img, extent=[-250, 250, -50, 420], zorder=0)
+
+plt.scatter(made_shots_x, made_shots_y, c='green', alpha=0.5, label='Made Shots')
+
+plt.title("Stephen Curry - Made Shots")
+plt.xlabel("Court X")
+plt.ylabel("Court Y")
+plt.legend()
+# plt.grid(True)
+plt.axis('equal')
+plt.show()
