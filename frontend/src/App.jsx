481da0cb709243f6e8b392d9e8bead03c9007283
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-04-09 19:17:52 -0400

    update to the styles of the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [players, setPlayers] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchPlayers = async () => {
@@ -21,40 +23,45 @@ function App() {
     fetchPlayers()
   }, [])
 
-  const handleSubmit = async (event) => {
+  const handleGenerateChart = async (event) => {
     event.preventDefault()
     try {
+      setLoading(true)
       const res = await fetch(`/api/player?firstname=${firstName}&lastname=${lastName}`)
       const blob = await res.blob()
       const url = URL.createObjectURL(blob)
       window.open(url) // or set state to render <img src={url} />
+      setLoading(false)
     } catch (error) {
       console.error('Error:', error)
+      setLoading(false)
     }
   }
   
   return (
     <div className='App'>
       <h1>NBA Shot Chart Analysis by Player</h1>
-      <h2>Players</h2>
-      <select 
-        onChange={(e) => {
-          const selectedId = parseInt(e.target.value)
-          const selectedPlayer = players.find(p => p.id === selectedId)
-          if (selectedPlayer) {
-            setFirstName(selectedPlayer.firstname)
-            setLastName(selectedPlayer.lastname)
-          }
-        }}
-      >
-        <option value=''>Select a player</option>
-        {players.map((player) => (
-          <option key={player.id} value={player.id}>
-            {player.firstname} {player.lastname}
-          </option>
-        ))}
-      </select>
-      <button onClick={handleSubmit}>Submit</button>
+      <div style={{display: "flex", flexDirection: "column", gap: "2rem"}}>
+        <select 
+          onChange={(e) => {
+            const selectedId = parseInt(e.target.value)
+            const selectedPlayer = players.find(p => p.id === selectedId)
+            if (selectedPlayer) {
+              setFirstName(selectedPlayer.firstname)
+              setLastName(selectedPlayer.lastname)
+            }
+          }}
+          style={{padding: "0.5rem 0 0.5rem 0", fontSize: "1rem"}}
+        >
+          <option value=''>Select a player</option>
+          {players.map((player) => (
+            <option key={player.id} value={player.id}>
+              {player.firstname} {player.lastname}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleGenerateChart} style={{width: "180px", alignSelf: "center"}}>{loading ? <CircularProgress size={18}/> : "Generate"}</button>
+      </div>
     </div>
   )
 }
