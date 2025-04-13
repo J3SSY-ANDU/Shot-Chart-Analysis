ebee5d5f4d64c5094af14deb53fa0ce984dda64d
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-04-12 23:26:59 -0400

    delete debugging lines

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,6 @@ def get_player(firstname: str = Query(...), lastname: str = Query(...)):
     all_shots = get_all_shots(player_id, team_id)
     made_x = [shot['LOC_X'] for shot in all_shots if shot['SHOT_MADE_FLAG'] == 1]
     made_y = [shot['LOC_Y'] for shot in all_shots if shot['SHOT_MADE_FLAG'] == 1]
-    print(len(made_x), len(all_shots))
 
     chart_path = shot_chart(made_x, made_y, all_shots, firstname, lastname)
     return FileResponse(chart_path, media_type="image/png")
